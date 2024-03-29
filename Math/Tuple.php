43d252a6b5beda06bf294b5260a5507e65bb49f5
Brimbard <brimbard@gmx.de>
2024-03-29 12:12:03 +0100

    Allow for alternative parameters on creation

--- Math/Tuple.php
+++ Math/Tuple.php
@@ -1,62 +1,113 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// | PHP Version 5                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Jesus M. Castagnetto <jmcastagnetto@php.net>                |
-// +----------------------------------------------------------------------+
-//
+/**
+ * Base class Tuple
+ * php version 5.0
+ *
+ * @category Math
+ * @package  Math_Vector
+ * @author   Jesus M. Castagnetto <jmcastagnetto@php.net>
+ * @license  https://www.php.net/license/2_02.txt PHP2.02
+ * @version  GIT: @0.9.0@ beta
+ * @link     https://github.com/pear/Math_Vector
+ *
+ * standards checked with CodeSniffer 3.9.0
+ * tested with PHPUnit 11.0.7 + PHP8.3
+ * Backwards compability: examples run in CLI with PHP5.6
+ *
+ * Old documentation:
+ * +----------------------------------------------------------------------+
+ * | PHP Version 5                                                        |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 1997-2003 The PHP Group                                |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the PHP license,       |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available at through the world-wide-web at                           |
+ * | http://www.php.net/license/2_02.txt.                                 |
+ * | If you did not receive a copy of the PHP license and are unable to   |
+ * | obtain it through the world-wide-web, please send a note to          |
+ * | license@php.net so we can mail you a copy immediately.               |
+ * +----------------------------------------------------------------------+
+ * | Authors: Jesus M. Castagnetto <jmcastagnetto@php.net>                |
+ * +----------------------------------------------------------------------+
+ */
+
 // $Id$
-//
+
 
 /**
  * General Tuple class
  * A Tuple represents a general unidimensional list of n numeric elements
  * Originally this class was part of NumPHP (Numeric PHP package)
  *
- * @author  Jesus M. Castagnetto <jmcastagnetto@php.net>
- * @version 1.0
- * @package Math_Vector
+ * @category Math
+ * @package  Math_Vector
+ * @author   Jesus M. Castagnetto <jmcastagnetto@php.net>
+ * @license  https://www.php.net/license/2_02.txt PHP2.02
+ * @version  Release: 0.9.0 beta
+ * @link     https://github.com/pear/Math_Vector
  */
 class Math_Tuple
 {
 
     /**
-     * array of numeric elements
+     * Array of numeric elements
      *
-     * @var     array
-     * @access  private
+     * @data array of elements
+     *
+     * @todo check if all elements are numeric
      */
-    var $data = null;
+    public $data = null;
 
     /**
      * Constructor of Math_Tuple
      *
-     * @param   array   $data   array of numbers
+     * This class is the data basis for the Vector class.
+     * It should only contain numeric values. For performance reasons
+     * not every element is checked on default.
+     * Use $this->isNumeric() if you deem necessary.
+     *
+     * @param ...$data array starting with a numeric
+     *                 list starting with a numeric
+     *                 empty
+     *                 tuple
+     *
+     * @throws InvalidArgumentException
      */
-    public function __construct($data)
+    public function __construct(...$data)
     {
-        if (is_array($data) || !is_array($data[0])) {
-            $this->data = $data;
+        $num_args = func_num_args();
+        $args = func_get_args();
+        if ($num_args < 1) {                    // no argument: empty
+            $this->data = array();
+        } elseif ($num_args < 2) {              // single argument
+            if (is_a($args[0], "Math_Tuple")) { // another Tuple
+                $this->data = $args[0]->data;
+            } elseif (is_array($args[0])) {     // an array
+                $this->data = $args[0];
+            } elseif (is_numeric($args[0])) {   // single numeric
+                $this->data = array($args[0]);
+            } else {
+                throw new InvalidArgumentException(
+                    "Unsupported parameter object."
+                );
+            }
+        } elseif (is_numeric($args[0])) {       // multiple arguments
+                                                // first is numeric
+            // Warning only testing 1. arg, so (1, 'foo') is accepted
+            // For performance reasons use $this->isNumeric() when needed
+            $this->data = $args;
         } else {
-            throw new InvalidArgumentException("An unidimensional array is needed to initialize a Tuple");
+            throw new InvalidArgumentException(
+                "Unrecognized parameters."
+            );
         }
     }
 
     /**
      * Squeezes out holes in the tuple sequence
      *
-     * @return  void
+     * @return void
      */
     public function squeezeHoles()
     {
@@ -66,7 +117,7 @@ class Math_Tuple
     /**
      * Returns the size (number of elements) in the tuple
      *
-     * @return  integer
+     * @return integer
      */
     public function getSize()
     {
@@ -76,16 +127,18 @@ class Math_Tuple
     /**
      * Sets the value of an element
      *
-     * @param   integer $elindex    element index
-     * @param   numeric $elvalue    element value
+     * @param integer $elindex element index
+     * @param numeric $elvalue element value
      *
-     * @return  mixed   true if successful
+     * @return mixed   true if successful
      * @throws InvalidArgumentException
      */
     public function setElement($elindex, $elvalue)
     {
         if ($elindex >= $this->getSize()) {
-            throw new InvalidArgumentException("Wrong index: $elindex for element: $elvalue");
+            throw new InvalidArgumentException(
+                "Wrong index: $elindex for element: $elvalue"
+            );
         }
         $this->data[$elindex] = $elvalue;
         return true;
@@ -94,13 +147,16 @@ class Math_Tuple
     /**
      * Appends an element to the tuple
      *
-     * @param   numeric $elvalue    element value
-     * @return  mixed   index of appended element on success
+     * @param numeric $elvalue element value
+     *
+     * @return mixed  index of appended element on success
      */
     public function addElement($elvalue)
     {
         if (!is_numeric($elvalue)) {
-            throw new InvalidArgumentException("Error, a numeric value is needed. You used: $elvalue");
+            throw new InvalidArgumentException(
+                "Error, a numeric value is needed. You used: $elvalue"
+            );
         }
         $this->data[$this->getSize()] = $elvalue;
         return ($this->getSize() - 1);
@@ -109,7 +165,7 @@ class Math_Tuple
     /**
      * Remove an element from the tuple
      *
-     * @param   integer $elindex    element index
+     * @param integer $elindex element index
      *
      * @return mixed   true on success
      * @throws InvalidArgumentException
@@ -117,7 +173,9 @@ class Math_Tuple
     public function delElement($elindex)
     {
         if ($elindex >= $this->getSize()) {
-            throw new InvalidArgumentException("Wrong index: $elindex, element not deleted");
+            throw new InvalidArgumentException(
+                "Wrong index: $elindex, element not deleted"
+            );
         }
 
         unset($this->data[$elindex]);
@@ -129,15 +187,17 @@ class Math_Tuple
     /**
      * Returns the value of an element in the tuple
      *
-     * @access  public
-     * @param   integer $elindex    element index
-     * @return  mixed   numeric on success
+     * @param integer $elindex element index
+     *
+     * @return mixed   numeric on success
      * @throws InvalidArgumentException
      */
     public function getElement($elindex)
     {
         if ($elindex >= $this->getSize()) {
-            throw new InvalidArgumentException("Wrong index: $elindex, Tuple size is: ".$this->getSize());
+            throw new InvalidArgumentException(
+                "Wrong index: $elindex, Tuple size is: ".$this->getSize()
+            );
         }
         return $this->data[$elindex];
     }
@@ -145,7 +205,7 @@ class Math_Tuple
     /**
      * Returns an array with all the elements of the tuple
      *
-     * @return  $array
+     * @return array
      */
     public function getData()
     {
@@ -156,8 +216,7 @@ class Math_Tuple
     /**
      * Returns the minimum value of the tuple
      *
-     * @access  public
-     * @return  numeric
+     * @return numeric
      */
     public function getMin()
     {
@@ -167,7 +226,7 @@ class Math_Tuple
     /**
      * Returns the maximum value of the tuple
      *
-     * @return  numeric
+     * @return numeric
      */
     public function getMax()
     {
@@ -177,19 +236,19 @@ class Math_Tuple
     /**
      * Returns an array of the minimum and maximum values of the tuple
      *
-     * @access  public
-     * @return  array of the minimum and maximum values
+     * @return array of the minimum and maximum values
      */
-    public function getMinMax ()
+    public function getMinMax()
     {
         return array ($this->getMin(), $this->getMax());
     }
 
     /**
-     * Gets the position of the given value in the tuple
+     * Gets the first position of the given value in the tuple
      *
-     * @param   numeric $val    value for which the index is requested
-     * @return  integer
+     * @param numeric $val value for which the index is requested
+     *
+     * @return mixed integer position or false
      */
     public function getValueIndex($val)
     {
@@ -204,7 +263,7 @@ class Math_Tuple
     /**
      * Gets the position of the minimum value in the tuple
      *
-     * @return  integer
+     * @return integer
      */
     public function getMinIndex()
     {
@@ -214,7 +273,7 @@ class Math_Tuple
     /**
      * Gets the position of the maximum value in the tuple
      *
-     * @return  integer
+     * @return integer
      */
     public function getMaxIndex()
     {
@@ -222,9 +281,10 @@ class Math_Tuple
     }
 
     /**
-     * Gets an array of the positions of the minimum and maximum values in the tuple
+     * Gets an array of the positions of the minimum
+     * and maximum values in the tuple
      *
-     * @return  array of integers indexes
+     * @return array of integers=indeces
      */
     public function getMinMaxIndex()
     {
@@ -234,7 +294,7 @@ class Math_Tuple
     /**
      * Checks if the tuple is a a Zero tuple
      *
-     * @return  boolean
+     * @return boolean
      */
     public function isZero()
     {
@@ -247,21 +307,37 @@ class Math_Tuple
     }
 
     /**
-     * Returns an string representation of the tuple
+     * Checks if the tuple only contains numeric values
      *
-     * @access  public
-     * @return  string
+     * It is possible to create a Tupe with a list or array(1, 'foo', ...)
+     * For performance reasons not every element is checked on default.
+     *
+     * @return boolean
      */
-    public function toString()
+    public function isNumeric()
     {
-        return "{ ".implode(", ",$this->data)." }";
+        for ($i=0; $i < $this->getSize(); $i++) {
+            if (!is_numeric($this->data[$i])) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
      * Returns an string representation of the tuple
      *
-     * @access  public
-     * @return  string
+     * @return string
+     */
+    public function toString()
+    {
+        return "{ ".implode(", ", $this->data)." }";
+    }
+
+    /**
+     * Returns an string representation of the tuple
+     *
+     * @return string
      */
     public function __toString()
     {
@@ -271,8 +347,7 @@ class Math_Tuple
     /**
      * Returns an HTML representation of the tuple
      *
-     * @access  public
-     * @return  string
+     * @return string
      */
     public function toHTML()
     {
